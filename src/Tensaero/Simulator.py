88d9d174ae2afaf8a88bd2075c54625c74788ae7
Caleb Morrison <cmorrison8191@gmail.com>
2025-10-26 17:34:06 -0700

    Add a stub for initializing sim objects

--- src/Tensaero/Simulator.py
+++ src/Tensaero/Simulator.py
@@ -1,6 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 from pathlib import Path
 import yaml
 from Tensaero.Core import Configuration
+from Tensaero.SimObjects import SimObjects
 
 
 class Simulator:
@@ -8,6 +13,10 @@ class Simulator:
         self.config_file_path = config_file_path
 
         self.config = self._load_and_validate_config()
+
+        self._sim_objects = {}
+
+        self._initialize_sim_objects()
 
 
     def _load_and_validate_config(self):
@@ -17,3 +26,20 @@ class Simulator:
         conf = Configuration.ConfigSchema(**conf)
 
         return conf
+
+
+    def _initialize_sim_objects(self):
+        for entry in self.config.sim_objects:
+            match entry.object_type:
+                case Configuration.SimObjectTypes.fixed_point:
+                    self._sim_objects[entry.name] = (
+                        SimObjects.FixedGroundPoint())
+                    # TODO: initialize object state
+                case Configuration.SimObjectTypes.general:
+                    continue
+                case Configuration.SimObjectTypes.ground:
+                    continue
+                case _:
+                    raise RuntimeError(f'Unknown SimObjectType:'
+                                       f' {entry.object_type}')
+
